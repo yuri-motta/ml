51ebef1d0e0a4eec7d1193bcf5a137bfa11584a5
Yuri Motta <ymlrs.eng@gmail.com>
2019-06-11 11:52:45 -0400

    Added new files related to unsupervised learning as well as a new model for titanic project based on kmeans

--- projects/titanic/visuals.py
+++ projects/titanic/visuals.py
@@ -9,148 +9,137 @@ from IPython import get_ipython
 get_ipython().run_line_magic('matplotlib', 'inline')
 ###########################################
 
+import matplotlib.pyplot as pl
+import matplotlib.patches as mpatches
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from time import time
+from sklearn.metrics import f1_score, accuracy_score
 
-def filter_data(data, condition):
-    """
-    Remove elements that do not match the condition provided.
-    Takes a data list as input and returns a filtered list.
-    Conditions should be a list of strings of the following format:
-      '<field> <op> <value>'
-    where the following operations are valid: >, <, >=, <=, ==, !=
-    
-    Example: ["Sex == 'male'", 'Age < 18']
-    """
 
-    field, op, value = condition.split(" ")
-    
-    # convert value into number or strip excess quotes if string
-    try:
-        value = float(value)
-    except:
-        value = value.strip("\'\"")
-    
-    # get booleans for filtering
-    if op == ">":
-        matches = data[field] > value
-    elif op == "<":
-        matches = data[field] < value
-    elif op == ">=":
-        matches = data[field] >= value
-    elif op == "<=":
-        matches = data[field] <= value
-    elif op == "==":
-        matches = data[field] == value
-    elif op == "!=":
-        matches = data[field] != value
-    else: # catch invalid operation codes
-        raise Exception("Invalid comparison operator. Only >, <, >=, <=, ==, != allowed.")
-    
-    # filter data and outcomes
-    data = data[matches].reset_index(drop = True)
-    return data
-
-def survival_stats(data, outcomes, key, filters = []):
+def distribution(data, transformed = False):
     """
-    Print out selected statistics regarding survival, given a feature of
-    interest and any number of filters (including no filters)
+    Visualization code for displaying skewed distributions of features
     """
     
-    # Check that the key exists
-    if key not in data.columns.values :
-        print("'{}' is not a feature of the Titanic data. Did you spell something wrong?".format(key))
-        return False
+    # Create figure
+    fig = pl.figure(figsize = (11,5));
 
-    # Return the function before visualizing if 'Cabin' or 'Ticket'
-    # is selected: too many unique categories to display
-    if(key == 'Cabin' or key == 'PassengerId' or key == 'Ticket'):
-        print("'{}' has too many unique categories to display! Try a different feature.".format(key))
-        return False
+    # Skewed feature plotting
+    for i, feature in enumerate(['capital-gain','capital-loss']):
+        ax = fig.add_subplot(1, 2, i+1)
+        ax.hist(data[feature], bins = 25, color = '#00A0A0')
+        ax.set_title("'%s' Feature Distribution"%(feature), fontsize = 14)
+        ax.set_xlabel("Value")
+        ax.set_ylabel("Number of Records")
+        ax.set_ylim((0, 2000))
+        ax.set_yticks([0, 500, 1000, 1500, 2000])
+        ax.set_yticklabels([0, 500, 1000, 1500, ">2000"])
 
-    # Merge data and outcomes into single dataframe
-    all_data = pd.concat([data, outcomes.to_frame()], axis = 1)
-    
-    # Apply filters to data
-    for condition in filters:
-        all_data = filter_data(all_data, condition)
-
-    # Create outcomes DataFrame
-    all_data = all_data[[key, 'Survived']]
-    
-    # Create plotting figure
-    plt.figure(figsize=(8,6))
-
-    # 'Numerical' features
-    if(key == 'Age' or key == 'Fare'):
-        
-        # Remove NaN values from Age data
-        all_data = all_data[~np.isnan(all_data[key])]
-        
-        # Divide the range of data into bins and count survival rates
-        min_value = all_data[key].min()
-        max_value = all_data[key].max()
-        value_range = max_value - min_value
-
-        # 'Fares' has larger range of values than 'Age' so create more bins
-        if(key == 'Fare'):
-            bins = np.arange(0, all_data['Fare'].max() + 20, 20)
-        if(key == 'Age'):
-            bins = np.arange(0, all_data['Age'].max() + 10, 10)
-        
-        # Overlay each bin's survival rates
-        nonsurv_vals = all_data[all_data['Survived'] == 0][key].reset_index(drop = True)
-        surv_vals = all_data[all_data['Survived'] == 1][key].reset_index(drop = True)
-        plt.hist(nonsurv_vals, bins = bins, alpha = 0.6,
-                 color = 'red', label = 'Did not survive')
-        plt.hist(surv_vals, bins = bins, alpha = 0.6,
-                 color = 'green', label = 'Survived')
-    
-        # Add legend to plot
-        plt.xlim(0, bins.max())
-        plt.legend(framealpha = 0.8)
-    
-    # 'Categorical' features
+    # Plot aesthetics
+    if transformed:
+        fig.suptitle("Log-transformed Distributions of Continuous Census Data Features", \
+            fontsize = 16, y = 1.03)
     else:
-       
-        # Set the various categories
-        if(key == 'Pclass'):
-            values = np.arange(1,4)
-        if(key == 'Parch' or key == 'SibSp'):
-            values = np.arange(0,np.max(data[key]) + 1)
-        if(key == 'Embarked'):
-            values = ['C', 'Q', 'S']
-        if(key == 'Sex'):
-            values = ['male', 'female']
+        fig.suptitle("Skewed Distributions of Continuous Census Data Features", \
+            fontsize = 16, y = 1.03)
 
-        # Create DataFrame containing categories and count of each
-        frame = pd.DataFrame(index = np.arange(len(values)), columns=(key,'Survived','NSurvived'))
-        for i, value in enumerate(values):
-            frame.loc[i] = [value, \
-                   len(all_data[(all_data['Survived'] == 1) & (all_data[key] == value)]), \
-                   len(all_data[(all_data['Survived'] == 0) & (all_data[key] == value)])]
+    fig.tight_layout()
+    fig.show()
 
-        # Set the width of each bar
-        bar_width = 0.4
 
-        # Display each category's survival rates
-        for i in np.arange(len(frame)):
-            nonsurv_bar = plt.bar(i-bar_width, frame.loc[i]['NSurvived'], width = bar_width, color = 'r')
-            surv_bar = plt.bar(i, frame.loc[i]['Survived'], width = bar_width, color = 'g')
+def evaluate(results, accuracy, f1):
+    """
+    Visualization code to display results of various learners.
+    
+    inputs:
+      - learners: a list of supervised learners
+      - stats: a list of dictionaries of the statistic results from 'train_predict()'
+      - accuracy: The score for the naive predictor
+      - f1: The score for the naive predictor
+    """
+  
+    # Create figure
+    fig, ax = pl.subplots(2, 4, figsize = (11,7))
 
-            plt.xticks(np.arange(len(frame)), values)
-            plt.legend((nonsurv_bar[0], surv_bar[0]),('Did not survive', 'Survived'), framealpha = 0.8)
+    # Constants
+    bar_width = 0.3
+    colors = ['#A00000','#00A0A0','#00A000']
+    
+    # Super loop to plot four panels of data
+    for k, learner in enumerate(results.keys()):
+        for j, metric in enumerate(['train_time', 'acc_train', 'f_train', 'pred_time', 'acc_test', 'f_test']):
+            for i in np.arange(3):
+                
+                # Creative plot code
+                ax[j//3, j%3].bar(i+k*bar_width, results[learner][i][metric], width = bar_width, color = colors[k])
+                ax[j//3, j%3].set_xticks([0.45, 1.45, 2.45])
+                ax[j//3, j%3].set_xticklabels(["1%", "10%", "100%"])
+                ax[j//3, j%3].set_xlabel("Training Set Size")
+                ax[j//3, j%3].set_xlim((-0.1, 3.0))
+    
+    # Add unique y-labels
+    ax[0, 0].set_ylabel("Time (in seconds)")
+    ax[0, 1].set_ylabel("Accuracy Score")
+    ax[0, 2].set_ylabel("F-score")
+    ax[1, 0].set_ylabel("Time (in seconds)")
+    ax[1, 1].set_ylabel("Accuracy Score")
+    ax[1, 2].set_ylabel("F-score")
+    
+    # Add titles
+    ax[0, 0].set_title("Model Training")
+    ax[0, 1].set_title("Accuracy Score on Training Subset")
+    ax[0, 2].set_title("F-score on Training Subset")
+    ax[1, 0].set_title("Model Predicting")
+    ax[1, 1].set_title("Accuracy Score on Testing Set")
+    ax[1, 2].set_title("F-score on Testing Set")
+    
+    # Add horizontal lines for naive predictors
+    ax[0, 1].axhline(y = accuracy, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
+    ax[1, 1].axhline(y = accuracy, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
+    ax[0, 2].axhline(y = f1, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
+    ax[1, 2].axhline(y = f1, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
+    
+    # Set y-limits for score panels
+    ax[0, 1].set_ylim((0, 1))
+    ax[0, 2].set_ylim((0, 1))
+    ax[1, 1].set_ylim((0, 1))
+    ax[1, 2].set_ylim((0, 1))
 
-    # Common attributes for plot formatting
-    plt.xlabel(key)
-    plt.ylabel('Number of Passengers')
-    plt.title('Passenger Survival Statistics With \'%s\' Feature'%(key))
-    plt.show()
+    # Set additional plots invisibles
+    ax[0, 3].set_visible(False)
+    ax[1, 3].axis('off')
 
-    # Report number of passengers with missing values
-    if sum(pd.isnull(all_data[key])):
-        nan_outcomes = all_data[pd.isnull(all_data[key])]['Survived']
-        print("Passengers with missing '{}' values: {} ({} survived, {} did not survive)".format( \
-              key, len(nan_outcomes), sum(nan_outcomes == 1), sum(nan_outcomes == 0)))
+    # Create legend
+    for i, learner in enumerate(results.keys()):
+        pl.bar(0, 0, color=colors[i], label=learner)
+    pl.legend()
+    
+    # Aesthetics
+    pl.suptitle("Performance Metrics for Three Supervised Learning Models", fontsize = 16, y = 1.10)
+    pl.tight_layout()
+    pl.show()
+    
 
+def feature_plot(importances, X_train, y_train):
+    
+    # Display the five most important features
+    indices = np.argsort(importances)[::-1]
+    columns = X_train.columns.values[indices[:5]]
+    values = importances[indices][:5]
+
+    # Creat the plot
+    fig = pl.figure(figsize = (9,5))
+    pl.title("Normalized Weights for First Five Most Predictive Features", fontsize = 16)
+    pl.bar(np.arange(5), values, width = 0.6, align="center", color = '#00A000', \
+          label = "Feature Weight")
+    pl.bar(np.arange(5) - 0.3, np.cumsum(values), width = 0.2, align = "center", color = '#00A0A0', \
+          label = "Cumulative Feature Weight")
+    pl.xticks(np.arange(5), columns)
+    pl.xlim((-0.5, 4.5))
+    pl.ylabel("Weight", fontsize = 12)
+    pl.xlabel("Feature", fontsize = 12)
+    
+    pl.legend(loc = 'upper center')
+    pl.tight_layout()
+    pl.show()  
